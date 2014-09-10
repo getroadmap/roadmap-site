15bf1928c17ad41b253298a97dd425be3f56ab56
Ben Kutil <ben@make-things.com>
2014-09-10 02:11:24 -0400

    initial commit

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'breakpoint'
+require 'susy'
+require 'sassy-maps'
+require 'compass/import-once/activate'
+
+# Set this to the root of your project when deployed:
+#http_path = ""
+project_path = "app/"
+css_dir = "styles"
+images_dir = "images"
+http_images_path = "/images"
+javascripts_dir = "scripts"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
