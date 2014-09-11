b4f5cfb0d2c7f7612e7883129d863b16b592e09f
Ben Kutil <ben@make-things.com>
2014-09-10 23:51:38 -0400

    basic mobile display

--- gulpfile.js
+++ gulpfile.js
@@ -183,7 +183,7 @@ gulp.task('pagespeed', pagespeed.bind(null, {
   // free (no API key) tier. You can use a Google
   // Developer API key if you have one. See
   // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://example.com',
+  url: 'http://getroadmap.github.io/roadmap-site/',
   strategy: 'mobile'
 }));
 
